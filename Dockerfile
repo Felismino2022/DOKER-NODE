05e3b1c4f5ced86e750ebd4ac25aa09f5e5a9ff6
Felismino2022 <muchombo2021@gmail.com>
2023-12-05 08:30:22 +0000

    update

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,10 @@ FROM node:alpine
 WORKDIR /usr/app
 
 COPY package*.json ./
+RUN npm install
 
 COPY . .
 
-EXPOSE 3000
+EXPOSE 3033
 
 CMD [ "npm", "start" ]
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const PORT = 3000
+const PORT = 3033
 const HOST = '0.0.0.0'
 
 app.get('/', (req, res) =>{
